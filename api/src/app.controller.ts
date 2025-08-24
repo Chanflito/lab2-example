190201b6f559c21be62a44676847a16ca9977f7e
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2025-08-24 02:50:19 -0300

    fix: update pre-commit hook and format app controller code

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("otro")getOtro(  ):string{
+    const obj={prop1:"value1",prop2:   "value2",prop3:"value3"};
+    if(obj.prop1==="value1"){return "formato horrible";}
+    return"otro   mal formateado";
   }
 }
