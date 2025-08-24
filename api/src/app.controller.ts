7fa3bc62267258c4aaee3f64f7bd0fcb15645092
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2025-08-24 17:15:39 -0300

    feat: upgrade NestJS and related dependencies to version 11.0.0; add Swagger documentation and global validation pipe
    refactor: remove unused 'getOtro' method from AppController
    
    feat: implement global error handling for PrismaClientKnownRequestError and enable CORS in main application

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -9,12 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('otro') getOtro(): string {
-    const obj = { prop1: 'value1', prop2: 'value2', prop3: 'value3' };
-    if (obj.prop1 === 'value1') {
-      return 'formato horrible';
-    }
-    return 'otro   mal formateado';
-  }
 }
