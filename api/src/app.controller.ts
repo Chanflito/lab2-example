4a0e189c8bd752e4eb32d41f8ec54d8963f08e9a
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2025-08-24 02:51:08 -0300

    run with lint-staged

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -11,9 +10,11 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("otro")getOtro(  ):string{
-    const obj={prop1:"value1",prop2:   "value2",prop3:"value3"};
-    if(obj.prop1==="value1"){return "formato horrible";}
-    return"otro   mal formateado";
+  @Get('otro') getOtro(): string {
+    const obj = { prop1: 'value1', prop2: 'value2', prop3: 'value3' };
+    if (obj.prop1 === 'value1') {
+      return 'formato horrible';
+    }
+    return 'otro   mal formateado';
   }
 }
